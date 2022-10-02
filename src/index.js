e42eafbd1681a1e7b13b10800bf27536f91882af
kevjinx <kevin415mei@gmail.com>
2022-10-01 20:12:49 -0700

    refactor to have state in board instead of square

--- src/index.js
+++ src/index.js
@@ -3,27 +3,42 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 class Square extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			value: null,
-		}
-	}
 
   render() {
     return (
-      <button className="square" onClick={
-				() => this.setState({value:'X'})
-			}>
-        {this.state.value}
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}
+			>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    // initializing board state
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
+
+  handleClick(i) {
+    //call slice() to create a shallow copy of squares to modify instead of modifying the existing array
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares})
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
