b9c1d65d65dcc70ca21ae5c8ddcc0814046c8cc7
kevjinx <kevin415mei@gmail.com>
2022-10-01 21:01:41 -0700

    add winner calculation and status

--- src/index.js
+++ src/index.js
@@ -16,6 +16,34 @@ import './index.css';
 //   }
 // }
 
+const calculateWinner = squares => {
+  const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < winningConditions.length; i++) {
+    const [a, b, c] = winningConditions[i];
+    if (
+      //square[a] to check for truthy value, since starting value is null
+      squares[a]
+      && squares[a] === squares[b]
+      && squares[b] === squares[c]
+      ) {
+        return squares[a]
+      }
+  }
+
+  return null;
+
+}
+
 const Square = props => {
   return (
     <button
@@ -61,7 +89,15 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = `Winner: ${winner}`
+    } else {
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    }
+
+
 
     return (
       <div>
@@ -101,6 +137,8 @@ class Game extends React.Component {
     );
   }
 }
+
+
 
 // ========================================
 
